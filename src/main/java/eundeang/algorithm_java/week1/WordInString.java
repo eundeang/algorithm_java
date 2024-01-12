f501032034a9ca07762cd919deb3420faafe6b7e
eundeang <111877048+eundeang@users.noreply.github.com>
2024-01-13 00:37:47 +0900

    String: 3. 문장 속 단어

---
+++ src/main/java/eundeang/algorithm_java/week1/WordInString.java
@@ -0,0 +1,28 @@
+package eundeang.algorithm_java.week1;
+
+import java.util.Scanner;
+
+public class WordInString {
+    public String solution(String st) {
+        String result = "";
+        String[] word = st.split(" ");
+        int longest = 0;
+        for (String s : word){
+            if (longest == s.length()){
+                result = word[0];
+            }else if(longest < s.length()) {
+                longest = s.length();
+                result = s;
+            }
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        WordInString wordInString = new WordInString();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(wordInString.solution(input));
+    }
+}
