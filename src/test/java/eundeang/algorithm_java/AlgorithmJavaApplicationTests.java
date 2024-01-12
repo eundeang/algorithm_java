e47b09873953377b25927c7bcc390f1caf0cd7ad
eundeang <111877048+eundeang@users.noreply.github.com>
2024-01-12 22:04:11 +0900

    init: 초기 프로젝트 파일 초기 생성

---
+++ src/test/java/eundeang/algorithm_java/AlgorithmJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package eundeang.algorithm_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
